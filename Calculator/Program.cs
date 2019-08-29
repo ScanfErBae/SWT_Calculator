8541273007fe52cf157e06417a68c5c243188de5
Frederik Salling Østergaard <201710715@post.au.dk>
2019-08-29 13:27:48 +0200

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Calc
+{
+    public double Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    public double Sub(double a, double b)
+    {
+        return a - b;
+    }
+
+    public double Multi(double a, double b)
+    {
+        return a * b;
+    }
+
+    public double Power(double x, double exp)
+    {
+        return Math.Pow(x, exp);
+    }
+
+}
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calc test = new Calc();
+            Console.WriteLine(test.Add(2, 2));
+            Console.WriteLine(test.Sub(4,2 ));
+            Console.WriteLine(test.Multi(3, 3));
+            Console.WriteLine(test.Power(3, 3));
+
+        }
+    }
+}
---
+++ Calculator_Test/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator_Test_Unit
+{     
+[TestFixture]
+    public class Class1
+    {
+        private Calc uut;
+        [SetUp]
+
+        public void Setup()
+        {
+           uut = new Calc();
+        }
+
+        [Test]
+        public void addCalculator_Test()
+        {
+            //Arrange
+            //Act
+            uut.Add(2,2);
+            //Assert
+            Assert.That(uut.Add(2,2), Is.EqualTo(4));
+        }
+
+        [Test]
+        public void MultiCalculator_Test()
+        {
+            //Arrange
+            //Act
+            uut.Multi(3, 3);
+            //Assert
+            Assert.That(uut.Multi(3, 3), Is.EqualTo(9));
+        }
+
+
+        [Test]
+        public void SubCalculator_Test()
+        {
+            //Arrange
+            //Act
+            uut.Sub(5, 3);
+            //Assert
+            Assert.That(uut.Sub(5, 3), Is.EqualTo(2));
+        }
+
+
+        [Test]
+        public void PowerCalculator_Test()
+        {
+            //Arrange
+            //Act
+            uut.Power(3, 3);
+            //Assert
+            Assert.That(uut.Power(3, 3), Is.EqualTo(27));
+        }
+
+    }
+}
